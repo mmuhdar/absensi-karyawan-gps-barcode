f6f70ad6c935eb9f029e43fa4c1b09f75426219b
mmuhdar <mmuhdar17@gmail.com>
2024-11-28 10:11:44 +0800

    update command

--- app/Console/Commands/CreateAbsensi.php
+++ app/Console/Commands/CreateAbsensi.php
@@ -28,24 +28,28 @@ class CreateAbsensi extends Command
      */
     public function handle()
     {
-        // Ambil user tertentu (misalnya berdasarkan ID)
-        $user = User::find("01jcsw880ez6dyjqncy2x5gwnm");
-        // $user = User::find("01jcyhwpvwabycz3qevkwxh14p");
+        $usersEmail = [
+            'mat@aws.com',
+            'testing@aws.com'
+        ];
+        $users = User::query()
+            ->whereIn('email', $usersEmail)
+            ->get();
 
-        // Buat absensi untuk user tersebut
-        Attendance::create([
-            'user_id' => $user->id,
-            'shift_id' => 2,
-            'barcode_id' => 1,  // Menambahkan tanggal hari ini
-            // 'time_in' => now()->format('H:i:s'),
-            'time_in' => '07:57:13',
-            'time_out' => '14:07:46',
-            'date' => now()->toDateString(),
-            'latitude' => 1.0353889776308,
-            'longitude' => 480.82285695705,
-            'status' => 'present',
-        ]);
-
-        $this->info('Absensi created for user ' . $user->name);
+        foreach ($users as $key => $value) {
+            Attendance::create([
+                'user_id' => $value->id,
+                'shift_id' => 2,
+                'barcode_id' => 1,
+                // 'time_in' => now()->format('H:i:s'),
+                'time_in' => '07:57:13',
+                'time_out' => '14:07:46',
+                'date' => now()->toDateString(),
+                'latitude' => 1.0353889776308,
+                'longitude' => 480.82285695705,
+                'status' => 'present',
+            ]);
+            $this->info('Absensi created for user ' . $value->name);
+        }
     }
 }
