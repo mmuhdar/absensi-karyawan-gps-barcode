3c57adda974c3b19387740e1cdb0cb4efceab5ee
mmuhdar <mmuhdar17@gmail.com>
2025-04-09 14:50:38 +0800

    adding auto time in and out for console create absent

--- app/Console/Commands/CreateAbsensi.php
+++ app/Console/Commands/CreateAbsensi.php
@@ -34,13 +34,23 @@ class CreateAbsensi extends Command
             'ileilham47@gmail.com',
             'andimohsidik01@gmail.com',
             'indraardiansyah2100@gmail.com',
-            'deswitawita100@gmail.com'
+            'deswitawita100@gmail.com',
         ];
         $users = User::query()
             ->whereIn('email', $usersEmail)
             ->get();
 
+        function randomTime($hour, $minStart, $minEnd)
+        {
+            $minute = rand($minStart, $minEnd);
+            $second = rand(0, 59);
+            return sprintf('%02d:%02d:%02d', $hour, $minute, $second);
+        }
+
         foreach ($users as $key => $value) {
+            $timeIn = randomTime(8, 0, 37);
+            $timeOut = randomTime(14, 0, 28);
+
             if (now()->dayOfWeek === 0) {
                 Attendance::create([
                     'user_id' => $value->id,
@@ -58,8 +68,8 @@ class CreateAbsensi extends Command
                     'user_id' => $value->id,
                     'shift_id' => 2,
                     'barcode_id' => 1,
-                    'time_in' => '07:57:13',
-                    'time_out' => '14:07:46',
+                    'time_in' => $timeIn,
+                    'time_out' => $timeOut,
                     'date' => now()->toDateString(),
                     'latitude' => 1.0353889776308,
                     'longitude' => 480.82285695705,
