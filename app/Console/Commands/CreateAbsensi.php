3ce6c26d08615770022c3c3dacbcf66944756709
mmuhdar <mmuhdar17@gmail.com>
2025-07-22 11:42:00 +0800

    minor

--- app/Console/Commands/CreateAbsensi.php
+++ app/Console/Commands/CreateAbsensi.php
@@ -31,9 +31,9 @@ class CreateAbsensi extends Command
         $usersEmail = [
             'mmuhdar08@gmail.com',
             'gomedsulas@gmail.com',
-            'ileilham47@gmail.com',
-            'andimohsidik01@gmail.com',
-            'indraardiansyah2100@gmail.com',
+            // 'ileilham47@gmail.com',
+            // 'andimohsidik01@gmail.com',
+            // 'indraardiansyah2100@gmail.com',
         ];
         $users = User::query()
             ->whereIn('email', $usersEmail)
